41b8923d2d44f94eb72b4274e06aa8a6a5b08cc8
manojkumars313 <manojraj.mm313@gmail.com>
2023-01-30 21:37:31 +0530

    prototypes

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,19 @@
+const parent = {
+  detail() {
+    console.log(
+      `My name is ${this.name} and i am from ${this.location} and my age is ${this.age}`
+    );
+  },
+};
+const firstChild = Object.create(parent);
+firstChild.name = "EA19";
+firstChild.location = "Bangalore";
+firstChild.age = "23";
+
+const firstSecond = Object.create(parent);
+firstSecond.name = "Manoj";
+firstSecond.location = "KA";
+firstSecond.age = "23";
+
+firstChild.detail();
+firstSecond.detail();
